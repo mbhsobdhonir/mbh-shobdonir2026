320cf42226e1a88edb033cc96818338ae8eb22c1
mbhsobdhonir <mdbelalbarguna1999@gmail.com>
2026-08-02 12:26:51 +0600

    Implement back-to-top button functionality
    Add functionality for a back-to-top button on scroll.

--- script.js
+++ script.js
@@ -114,3 +114,25 @@ nav.classList.toggle("active");
 });
 
 }
+// ===== Back To Top =====
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
